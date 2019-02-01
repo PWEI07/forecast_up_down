061174ea0ef43051af05422fdbed855503d902fb
Zirui Wei <919582943@qq.com>
2019-02-01 15:51:53 +0800

    keras.GRU model with future data for predicting 50ETF

--- rnn0130.py
+++ rnn0130.py
@@ -9,32 +9,31 @@ from sklearn.preprocessing import MinMaxScaler
 
 rd.init()
 
-spot_data1 = rd.get_price('510050.XSHG', start_date='2014-04-01', end_date='2019-01-29', frequency='1d')
-# future_data1 = rd.get_price('IH88', start_date='2015-06-01', end_date='2019-01-29', frequency='1d')
+spot_data1 = rd.get_price('510050.XSHG', start_date='2015-06-01', end_date='2019-01-29', frequency='1d')
+future_data1 = rd.get_price('IH88', start_date='2015-06-01', end_date='2019-01-29', frequency='1d')
 
 spot_data1 = pd.DataFrame(spot_data1)
-# future_data1 = pd.DataFrame(future_data1)
+future_data1 = pd.DataFrame(future_data1)
 # future_data1都是50ETF期货主力合约信息，最好能把主力合约距离到期日的天数囊括进去
-# future_data1['dominant'] = rd.futures.get_dominant('IF', start_date='2015-06-01', end_date='2019-01-29')
-# future_data1['delisted_date'] = list(map(lambda x: datetime.datetime.strptime(rd.instruments(x).de_listed_date,
-#                                                                               '%Y-%m-%d'), future_data1['dominant']))
-# future_data1['remaining_days'] = future_data1['delisted_date'] - future_data1.index
-# future_data1['remaining_days'] = list(map(lambda x: x.days, future_data1['remaining_days']))
-# future_data1.drop(columns=['dominant', 'delisted_date'], inplace=True)
+future_data1['dominant'] = rd.futures.get_dominant('IF', start_date='2015-06-01', end_date='2019-01-29')
+future_data1['delisted_date'] = list(map(lambda x: datetime.datetime.strptime(rd.instruments(x).de_listed_date,
+                                                                              '%Y-%m-%d'), future_data1['dominant']))
+future_data1['remaining_days'] = future_data1['delisted_date'] - future_data1.index
+future_data1['remaining_days'] = list(map(lambda x: x.days, future_data1['remaining_days']))
+future_data1.drop(columns=['dominant', 'delisted_date'], inplace=True)
 
 # 获取滞后了1天的数据
-spot_data2 = rd.get_price('510050.XSHG', start_date='2014-04-02', end_date='2019-01-30', frequency='1d')
+spot_data2 = rd.get_price('510050.XSHG', start_date='2015-06-02', end_date='2019-01-30', frequency='1d')
 y_data = spot_data2['close'].values > spot_data1['close'].values
 y_data = to_categorical(y_data)  # 第一列代表是否跌，第二列代表是否涨
-# spot_data1.columns = list(map(lambda x: 'spot_' + x, spot_data1.columns))
-# future_data1.columns = list(map(lambda x: 'future_' + x, future_data1.columns))
-# x_data = pd.DataFrame.merge(spot_data1, future_data1, left_index=True, right_index=True)
+spot_data1.columns = list(map(lambda x: 'spot_' + x, spot_data1.columns))
+future_data1.columns = list(map(lambda x: 'future_' + x, future_data1.columns))
+x_data = pd.DataFrame.merge(spot_data1, future_data1, left_index=True, right_index=True)
 
-# x_data.iloc[1:, :-1] = x_data.iloc[1:, :-1].values / x_data.iloc[:-1, :-1].values - 1
-# x_data.iloc[0, :-1] = 0
+
 # 数据处理完毕，接下来就是丢进模型了！
 
-x_data = spot_data1.copy()
+# x_data = spot_data1.copy()
 # 对数据进行划分
 shift_days = 1
 num_data = len(x_data)
@@ -49,8 +48,6 @@ x_train = x_data[0:num_train]
 x_vali = x_data[num_train:num_train + num_vali]
 x_test = x_data[num_train + num_vali:]
 
-# y_data = np.array(y_data)
-# y_data = y_data.reshape(-1, 1)
 y_train = y_data[0:num_train]
 y_vali = y_data[num_train:num_train + num_vali]
 y_test = y_data[num_train + num_vali:]
@@ -95,7 +92,7 @@ def batch_generator(batch_size, sequence_length):
 
 
 batch_size = 8
-sequence_length = 25
+sequence_length = 15
 
 generator = batch_generator(batch_size=batch_size,
                             sequence_length=sequence_length)
@@ -117,19 +114,19 @@ model.add(keras.layers.GRU(units=6, input_shape=(None, num_x_signals,),
                            kernel_initializer=init,
                            kernel_regularizer=keras.regularizers.l1(0.001)))
 
-# model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.Dense(num_y_signals * 3,
                              activation='relu',
                              kernel_initializer=init,
                              kernel_regularizer=keras.regularizers.l1(0.01)))
 
-# model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.BatchNormalization())
 #
-# model.add(keras.layers.Dense(num_y_signals * 2,
-#                              activation='relu',
-#                              kernel_initializer=init,
-#                              kernel_regularizer=keras.regularizers.l1(0.01)))
+model.add(keras.layers.Dense(num_y_signals * 2,
+                             activation='relu',
+                             kernel_initializer=init,
+                             kernel_regularizer=keras.regularizers.l1(0.01)))
 
 model.add(keras.layers.BatchNormalization())
 
@@ -164,24 +161,52 @@ callbacks = [callback_early_stopping,
              callback_tensorboard,
              callback_reduce_lr]
 
-model.fit_generator(generator=generator,
+history = model.fit_generator(generator=generator,
                     epochs=100,
-                    steps_per_epoch=2,
+                    steps_per_epoch=16,
                     validation_data=validation_data,
                     callbacks=callbacks)
 
 model.load_weights(path_checkpoint)
 
 
+test_set_prediction = model.predict_classes(np.expand_dims(x_test_scaled, axis=0))[0]
+true_test_label = np.argmax(y_test, axis=1)
 
 
-from keras import backend as K
 
-inp = model.input                                           # input placeholder
-outputs = [layer.output for layer in model.layers]          # all layer outputs
-functors = [K.function([inp], [out]) for out in outputs]    # evaluation functions
 
-# Testing
-test = np.expand_dims(x_data, axis=0)
-layer_outs = [func([test]) for func in functors]
-print(layer_outs)
+
+# accuracy when market go down
+np.sum(true_test_label[true_test_label == 0] == test_set_prediction[true_test_label == 0]) / len(true_test_label[true_test_label == 0])
+# 2 / 126 = 0.016
+
+# accuracy when market go up
+np.sum(true_test_label[true_test_label == 1] == test_set_prediction[true_test_label == 1]) / len(true_test_label[true_test_label == 1])
+# 109 / 111 = 0.982
+
+# accuracy when predicting market will go down
+np.sum(true_test_label[test_set_prediction == 0] == test_set_prediction[test_set_prediction == 0]) / len(test_set_prediction[test_set_prediction == 0])
+# 2 / 4 = 0.5
+
+# accuracy when prediction market will go up
+np.sum(true_test_label[test_set_prediction == 1] == test_set_prediction[test_set_prediction == 1]) / len(test_set_prediction[test_set_prediction == 1])
+# 109 / 233 = 0.468
+
+# overall accuracy
+np.sum(true_test_label == test_set_prediction) / len(true_test_label)
+# 0.468
+
+
+
+
+# from keras import backend as K
+#
+# inp = model.input                                           # input placeholder
+# outputs = [layer.output for layer in model.layers]          # all layer outputs
+# functors = [K.function([inp], [out]) for out in outputs]    # evaluation functions
+#
+# # Testing
+# test = np.expand_dims(x_data, axis=0)
+# layer_outs = [func([test]) for func in functors]
+# print(layer_outs)
